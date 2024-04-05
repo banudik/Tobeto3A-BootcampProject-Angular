0a564f4ac8e9ad397053e82f6fddd43c63de6785
Banu Dik <banudik_34@hotmail.com>
2024-04-06 02:03:50 +0300

    06.04.24

---
+++ src/app/core/guards/login/login.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
+import { AuthService } from '../../../features/services/concretes/auth.service';
+
+
+export const LoginGuard:CanActivateFn=
+(route:ActivatedRouteSnapshot,state:RouterStateSnapshot)=>{
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if(authService.loggedIn()){return true;}
+  else{router.navigate(["register"]);
+   alert("Giriş yapmalısınız!")
+   return false}
+}
+
+    
