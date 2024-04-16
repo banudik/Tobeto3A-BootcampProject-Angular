99f393469deb2195a71f827ee7951a53a526160a
Ege Umut Tali <egeumuttali@hotmail.com>
2024-04-16 12:08:55 +0300

    Added method GetApplicantById for MyProfile Page and edited necessary parts. it works but page design needs a refactor

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ApplicantService } from '../../features/services/concretes/applicant.service';
+import { GetByIdApplicantResponse } from '../../features/models/responses/applicant/get-by-id-applicant-response';
 
 @Component({
   selector: 'app-profile',
@@ -8,8 +10,30 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
+  getApplicantByIdResponse!:GetByIdApplicantResponse
 
-  constructor(private router:Router) {}
-}
+  constructor(private router:Router, private activatedRoute: ActivatedRoute, private applicantService:ApplicantService) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: { [x: string]: string; }) => {
+      if (params["userId"]) {
+        this.getApplicantById(params["userId"])
+      } else { console.log("getById applicant error") }
+    })
+  }
+
+  getApplicantById(applicantId: string): void {
+    this.applicantService.getApplicantById(applicantId).subscribe(
+      (response: GetByIdApplicantResponse) => {
+        console.log("geliyor " + response.email);
+        this.getApplicantByIdResponse = response;
+      },
+      (error: any) => {
+        console.error('Error fetching applicant:', error);
+        // Hata işleme mekanizmasını buraya ekleyebilirsiniz
+      }
+    );
+  }
+} 
