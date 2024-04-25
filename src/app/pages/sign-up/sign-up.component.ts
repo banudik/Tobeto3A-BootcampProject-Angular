46df71944897b187ee9d29e90fe606844dae4b93
Berker ERTAN <berkerertan@hotmail.com>
2024-04-26 00:02:48 +0300

    Client-Side Register Validation added

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../features/services/concretes/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterModule,ReactiveFormsModule],
+  imports: [RouterModule,ReactiveFormsModule,CommonModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
@@ -26,8 +27,15 @@ export class SignUpComponent implements OnInit {
    this.registerForm=this.formBuilder.group({
     firstName:["",Validators.required],  
     lastName:["",Validators.required],  
-    email:["",Validators.required],
-    password:["",Validators.required],
+    email:["",[Validators.required, Validators.email]],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/.*[A-Z].*/), // Büyük harf
+      Validators.pattern(/.*[a-z].*/), // küçük harf
+      Validators.pattern(/.*[0-9].*/), // sayı
+      Validators.pattern(/.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+.*/) // özel karakter
+    ]],
     about:["",Validators.required],
     dateOfBirth:["",Validators.required],
     userName:["",Validators.required],
