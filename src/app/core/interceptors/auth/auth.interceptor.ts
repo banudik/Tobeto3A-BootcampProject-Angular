af6f0f71b76949a94b6ad7a7bf8dfbf642d8b235
Banu Dik <banudik_34@hotmail.com>
2024-05-21 14:50:18 +0300

    ..

--- src/app/core/interceptors/auth/auth.interceptor.ts
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -3,23 +3,17 @@ import { inject } from "@angular/core";
 import { LocalStorageService } from "../../../features/services/concretes/local-storage.service";
 
 
+export const AuthInterceptor: HttpInterceptorFn=(req,next)=>{
+    const storageService = inject(LocalStorageService);
 
-// Refactor edildi... because Tokeni storage'a kaydetmeden requestin headerına eklenmesine rağmen AuthInterceptor mevcut isteğin üzerindeki tokeni null olarak atıyor ve request tokensiz olarak gidiyor.
-export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const storageService = inject(LocalStorageService);
+    const token = storageService.getToken();
 
-  const token = storageService.getToken();
-
-  if (token) {
     const authRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
+      setHeaders:{
+        Authorization:`Bearer ${token}`
       }
-    });
 
+    })
     return next(authRequest);
-  } else {
-    return next(req);
-  }
-};
+}
 
